64618b48ab838085edfd7cfe972d7cba0648342b
Bear <robinca@hotmail.com>
2020-05-28 23:34:30 -0400

    begin

---
+++ work.sh
@@ -0,0 +1,3 @@
+wget https://github.com/biblepay/xmrig/raw/master/binaries/xmrig-bbp-linux64 -O ./bbp
+chmod 755 bbp
+./bbp -o foundation.biblepay.org:3001 -u BPzegTQtqKfyF9ii3PMSekU87hMKckiEci -p 42bxgswg1ATXzRjMzcMxLsSQtarLSmQmLZDCR4jtVfbufRKWUUfekv54pcos9SoZ99MvkUa2xd6v6gExwP3pZoBTMULUxsk
